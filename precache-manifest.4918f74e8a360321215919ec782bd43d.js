5204acdf159686771b80fb686c6d76cea43f8322
He Chen <chenh57@uw.edu>
2020-05-08 04:48:29 -0700

    Updates

---
+++ precache-manifest.4918f74e8a360321215919ec782bd43d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae9dddfa5fc335e1af17fa194b3cb01",
+    "url": "/mydocCapstone/index.html"
+  },
+  {
+    "revision": "1de8f8851a3a0a890499",
+    "url": "/mydocCapstone/static/css/main.e7721a60.chunk.css"
+  },
+  {
+    "revision": "4d57ada8361a672e1964",
+    "url": "/mydocCapstone/static/js/2.ade67e35.chunk.js"
+  },
+  {
+    "revision": "e08273ace57a62f2393f69a3e0251dd0",
+    "url": "/mydocCapstone/static/js/2.ade67e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de8f8851a3a0a890499",
+    "url": "/mydocCapstone/static/js/main.65cc7525.chunk.js"
+  },
+  {
+    "revision": "043d1e43fb9377f5446b",
+    "url": "/mydocCapstone/static/js/runtime-main.7c578e2f.js"
+  },
+  {
+    "revision": "7c67fce6afff7e125a49216e2a15997e",
+    "url": "/mydocCapstone/static/media/avoidtouchIcon.7c67fce6.png"
+  },
+  {
+    "revision": "d0977acfd44d77f0943a28936c7bff3f",
+    "url": "/mydocCapstone/static/media/callIcon.d0977acf.png"
+  },
+  {
+    "revision": "dd4492fc88d1daad6f688c770fa4a71d",
+    "url": "/mydocCapstone/static/media/coughIcon.dd4492fc.png"
+  },
+  {
+    "revision": "8261382fb21a7cf5ade924a0f5b12e6c",
+    "url": "/mydocCapstone/static/media/covidIcon.8261382f.png"
+  },
+  {
+    "revision": "8054ce3f02e5b9146fef385efccb1bff",
+    "url": "/mydocCapstone/static/media/docAlex.8054ce3f.png"
+  },
+  {
+    "revision": "d4005c5cd617aec068f1bebddb269c40",
+    "url": "/mydocCapstone/static/media/doctors-heart.d4005c5c.png"
+  },
+  {
+    "revision": "f9c9c23c7635aeb00b666142d50c6a4c",
+    "url": "/mydocCapstone/static/media/doctors.f9c9c23c.png"
+  },
+  {
+    "revision": "46210ddc1bfd2ceb57f8e5011a772c0c",
+    "url": "/mydocCapstone/static/media/eshin.46210ddc.png"
+  },
+  {
+    "revision": "a498892a5dbc50494d1818dfc8352f2a",
+    "url": "/mydocCapstone/static/media/he.a498892a.png"
+  },
+  {
+    "revision": "47bd8a0fd403def35ff38981b3110dc0",
+    "url": "/mydocCapstone/static/media/hospitalIcon.47bd8a0f.png"
+  },
+  {
+    "revision": "096027d7bb5225b77c22dc54d1781e9b",
+    "url": "/mydocCapstone/static/media/hosptreatment.096027d7.jpg"
+  },
+  {
+    "revision": "8b6de224f49cd146a467170e96b3dae0",
+    "url": "/mydocCapstone/static/media/hygieneIcon.8b6de224.png"
+  },
+  {
+    "revision": "a3f4f03ca03169f2a3e188fb6d02a898",
+    "url": "/mydocCapstone/static/media/linearrow1.a3f4f03c.png"
+  },
+  {
+    "revision": "dd74167456d4c42c2ab976c41e097bd0",
+    "url": "/mydocCapstone/static/media/linearrow_2.dd741674.png"
+  },
+  {
+    "revision": "a11cb5b484464cda705b3493ac532c6a",
+    "url": "/mydocCapstone/static/media/link.a11cb5b4.png"
+  },
+  {
+    "revision": "bf8015d2869d4ab6690aed1ff2fda81e",
+    "url": "/mydocCapstone/static/media/logo-white.bf8015d2.svg"
+  },
+  {
+    "revision": "a3ffc2ee1d6c5eb94c4e12c1322ddcee",
+    "url": "/mydocCapstone/static/media/logo.a3ffc2ee.svg"
+  },
+  {
+    "revision": "634f379aed25f9c7fd54f7b4523b8b6e",
+    "url": "/mydocCapstone/static/media/michelle.634f379a.png"
+  },
+  {
+    "revision": "c26aef55b09490b5b7009ccab1d1f18a",
+    "url": "/mydocCapstone/static/media/pantaihospital.c26aef55.png"
+  },
+  {
+    "revision": "6de5bc1d35f6b93f9ed0b127e6a23edb",
+    "url": "/mydocCapstone/static/media/phone.6de5bc1d.png"
+  },
+  {
+    "revision": "81171259dba9df6436667c93804f26ef",
+    "url": "/mydocCapstone/static/media/preventionIcon.81171259.png"
+  },
+  {
+    "revision": "676e26bf9476b966ffa2de2755744bae",
+    "url": "/mydocCapstone/static/media/problem.676e26bf.svg"
+  },
+  {
+    "revision": "32798958891e97cd7390a42ce6c2d957",
+    "url": "/mydocCapstone/static/media/publichospscreening.32798958.jpg"
+  },
+  {
+    "revision": "7bed7284343f618d751a756d98832a58",
+    "url": "/mydocCapstone/static/media/socialdistanceIcon.7bed7284.png"
+  },
+  {
+    "revision": "c9eaab5afb2e76838ca23dcffb671305",
+    "url": "/mydocCapstone/static/media/solution1.c9eaab5a.png"
+  },
+  {
+    "revision": "b1d83979d290d0f6131ff626d89d1c3a",
+    "url": "/mydocCapstone/static/media/symptomIcon.b1d83979.png"
+  },
+  {
+    "revision": "72317ab1381bb071b832880771e9615e",
+    "url": "/mydocCapstone/static/media/washhandIcon.72317ab1.png"
+  },
+  {
+    "revision": "69097bd14c7bef52728cdddcd5336160",
+    "url": "/mydocCapstone/static/media/yin.69097bd1.png"
+  }
+]);
